8c10d6f049f79721084a866debea0ca1704daac3
Yun Ji <this.jiyun@gmail.com>
2024-11-23 13:07:06 +0200

    chroe:use production api

--- src/misc/utils.ts
+++ src/misc/utils.ts
@@ -72,10 +72,9 @@ export const setParams = (
 }
 
 
-//export const API_URL =
-//  'https://catagonia-api-bgexcxfef5e0dqhh.northeurope-01.azurewebsites.net/api/v1'
+export const API_URL =
+  'https://catagonia-api-bgexcxfef5e0dqhh.northeurope-01.azurewebsites.net/api/v1'
 
-export const API_URL = 'http://localhost:5233/api/v1'
 
 export const parseOrderStatus = (status: number) => {
   if (status === 0) {
